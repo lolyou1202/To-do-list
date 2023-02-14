2714a6173577bafb1b647fa1cedbd3c9d6836c5d
lolyou1202 <97385179+lolyou1202@users.noreply.github.com>
2023-02-14 22:46:17 +0300

    заверстал начальную страницу

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { ReactSVG } from "react-svg";
 
 export default function App() {
+
   return (
     <div className="App">
       <header className="header">
@@ -21,8 +22,190 @@ export default function App() {
             src={require('../src/Image/search-ico.svg').default}
           />
         </div>
+        <ul className="stages">
+          <li className="stages__item stages__undone active">
+            <p>Undone</p>
+          </li>
+          <li className="stages__item stages__meetings">
+            <p>Meetings</p>
+          </li>
+          <li className="stages__item stages__consummation">
+            <p>Consummation</p>
+          </li>
+        </ul>
+        <div className="toDo__list">
+          <div className="toDo__task">
+            <div className="task-content">
+              <h2 className="task-name">Project daily stand-up</h2>
+              <p className="task-description">At the conference center</p>
+            </div>
+            <div className="task-priority"></div>
+            <div className="task-colorMark"></div>
+            <div className="task-contact">
+              <span className="task-time">9:00 am</span>
+              <ul className="task-invited">
+                <li className="task-invited-person"></li>
+                <li className="task-invited-person"></li>
+                <li className="task-invited-person"></li>
+              </ul>
+            </div>
+          </div>
+          <div className="toDo__task">
+            <div className="task-content">
+              <h2 className="task-name">Project daily stand-up</h2>
+              <p className="task-description">At the conference center</p>
+            </div>
+            <div className="task-priority"></div>
+            <div className="task-colorMark"></div>
+            <div className="task-contact">
+              <span className="task-time">9:00 am</span>
+              <ul className="task-invited">
+                <li className="task-invited-person"></li>
+                <li className="task-invited-person"></li>
+                <li className="task-invited-person"></li>
+              </ul>
+            </div>
+          </div>
+          <div className="toDo__task">
+            <div className="task-content">
+              <h2 className="task-name">Project daily stand-up</h2>
+              <p className="task-description">At the conference center</p>
+            </div>
+            <div className="task-priority"></div>
+            <div className="task-colorMark"></div>
+            <div className="task-contact">
+              <span className="task-time">9:00 am</span>
+              <ul className="task-invited">
+                <li className="task-invited-person"></li>
+                <li className="task-invited-person"></li>
+                <li className="task-invited-person"></li>
+              </ul>
+            </div>
+          </div>
+          <div className="toDo__task">
+            <div className="task-content">
+              <h2 className="task-name">Project daily stand-up</h2>
+              <p className="task-description">At the conference center</p>
+            </div>
+            <div className="task-priority"></div>
+            <div className="task-colorMark"></div>
+            <div className="task-contact">
+              <span className="task-time">9:00 am</span>
+              <ul className="task-invited">
+                <li className="task-invited-person"></li>
+                <li className="task-invited-person"></li>
+                <li className="task-invited-person"></li>
+              </ul>
+            </div>
+          </div>
+          <div className="toDo__task">
+            <div className="task-content">
+              <h2 className="task-name">Project daily stand-up</h2>
+              <p className="task-description">At the conference center</p>
+            </div>
+            <div className="task-priority"></div>
+            <div className="task-colorMark"></div>
+            <div className="task-contact">
+              <span className="task-time">9:00 am</span>
+              <ul className="task-invited">
+                <li className="task-invited-person"></li>
+                <li className="task-invited-person"></li>
+                <li className="task-invited-person"></li>
+              </ul>
+            </div>
+          </div>
+          <div className="toDo__task">
+            <div className="task-content">
+              <h2 className="task-name">Project daily stand-up</h2>
+              <p className="task-description">At the conference center</p>
+            </div>
+            <div className="task-priority"></div>
+            <div className="task-colorMark"></div>
+            <div className="task-contact">
+              <span className="task-time">9:00 am</span>
+              <ul className="task-invited">
+                <li className="task-invited-person"></li>
+                <li className="task-invited-person"></li>
+                <li className="task-invited-person"></li>
+              </ul>
+            </div>
+          </div>
+          <div className="toDo__task">
+            <div className="task-content">
+              <h2 className="task-name">Project daily stand-up</h2>
+              <p className="task-description">At the conference center</p>
+            </div>
+            <div className="task-priority"></div>
+            <div className="task-colorMark"></div>
+            <div className="task-contact">
+              <span className="task-time">9:00 am</span>
+              <ul className="task-invited">
+                <li className="task-invited-person"></li>
+                <li className="task-invited-person"></li>
+                <li className="task-invited-person"></li>
+              </ul>
+            </div>
+          </div>
+          <div className="toDo__task">
+            <div className="task-content">
+              <h2 className="task-name">Project daily stand-up</h2>
+              <p className="task-description">At the conference center</p>
+            </div>
+            <div className="task-priority"></div>
+            <div className="task-colorMark"></div>
+            <div className="task-contact">
+              <span className="task-time">9:00 am</span>
+              <ul className="task-invited">
+                <li className="task-invited-person"></li>
+                <li className="task-invited-person"></li>
+                <li className="task-invited-person"></li>
+              </ul>
+            </div>
+          </div>
+          <div className="toDo__task">
+            <div className="task-content">
+              <h2 className="task-name">Project daily stand-up</h2>
+              <p className="task-description">At the conference center</p>
+            </div>
+            <div className="task-priority"></div>
+            <div className="task-colorMark"></div>
+            <div className="task-contact">
+              <span className="task-time">9:00 am</span>
+              <ul className="task-invited">
+                <li className="task-invited-person"></li>
+                <li className="task-invited-person"></li>
+                <li className="task-invited-person"></li>
+              </ul>
+            </div>
+          </div>
+          <div className="toDo__task">
+            <div className="task-content">
+              <h2 className="task-name">Project daily stand-up</h2>
+              <p className="task-description">At the conference center</p>
+            </div>
+            <div className="task-priority"></div>
+            <div className="task-colorMark"></div>
+            <div className="task-contact">
+              <span className="task-time">9:00 am</span>
+              <ul className="task-invited">
+                <li className="task-invited-person"></li>
+                <li className="task-invited-person"></li>
+                <li className="task-invited-person"></li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </main>
-      <div className="newTask"></div>
+      <button className="newTask">
+        <ReactSVG
+          className="plus-ico"
+          src={require('../src/Image/plus-ico.svg').default}
+        />
+        <p>Add new task</p>
+      </button>
+      <div className="newTask-modal">
+        
+      </div>
     </div>
   );
 }
